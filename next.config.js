7df07ae54057d105161ba5958965e0030fb54e99
QuanttumQuokka <arushjain9557@gmail.com>
2026-02-10 01:27:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath:'/project-for-20_03_2004-her',
-  assetPrefix:'/project-for-20_03_2004-her/',
   images: {
     unoptimized: true,
   },
